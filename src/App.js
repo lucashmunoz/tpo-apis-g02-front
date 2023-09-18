5acfc8409d6f746108fe238da3c31690645cdcc4
lucashmunoz <lucashmunoz@gmail.com>
2023-09-17 21:31:16 -0300

    se instala tailwind y se crea Button.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      TPO
-    </div>
+    <Home />
   );
 }
 
---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,26 @@
+
+const Button = ({ children, buttonType }) => {
+
+    const getButtonClassName = () => {
+        const classNamePrimary = "bg-green-500 text-white hover:bg-green-600 py-2 px-4 rounded";
+        const classNameSecondary = "bg-white border border-black rounded-lg hover:bg-gray-200 py-2 px-4";
+
+        switch (buttonType) {
+            case "primary":
+                return classNamePrimary;
+            case "secondary":
+                return classNameSecondary;
+            default:
+                return "";
+        }
+    }
+
+
+    return (
+        <button className={getButtonClassName()} >
+            {children}
+        </button >
+    )
+}
+
+export default Button; 
