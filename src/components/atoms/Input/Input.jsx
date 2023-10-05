0ea292f67894d841701fd5165286a0bb66a27a7f
lucashmunoz <lucashmunoz@gmail.com>
2023-10-05 02:48:48 -0300

    Primera version pagina detalle servicio

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
-const Label = styled.label``;
+const HorizontalSpace = styled.div`
+  height: 5px;
+`;
 
 const InputTag = styled.input`
-  margin-top: 5px;
   border: solid
     ${(props) => (props.$inputType === "primary" ? "2px #22c55e" : "1px black")};
   width: 100%;
   border-radius: 5px;
   padding: 8px 16px;
+
+  &:hover {
+    border-color: #22c55e;
+  }
 `;
 
 const Input = ({
@@ -21,7 +26,8 @@ const Input = ({
 }) => {
   return (
     <>
-      <Label htmlFor={id}>{labelText}</Label>
+      <label htmlFor={id}>{labelText}</label>
+      {labelText && <HorizontalSpace />}
       <InputTag
         id={id}
         type="text"
