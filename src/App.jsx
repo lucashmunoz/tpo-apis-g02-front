0624b7f4726d46d3a5eb49b22add59f597e656e8
Ignacio Cesarani <ignaciocesarani2001@gmail.com>
2023-10-05 19:52:28 -0300

    login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "components/molecules/Footer";
 import Header from "components/molecules/Header";
+import Login from "pages/Login";
+import Register from "pages/Register";
 import Home from "pages/Home";
 import SobreNosotros from "pages/SobreNosotros";
 import Services from "pages/Services";
@@ -21,6 +23,8 @@ function App() {
             <Route path="/sobrenosotros" element={<SobreNosotros />} />
             <Route path="/services" element={<Services />} />
             <Route path="/service/:id" element={<ServiceDetail />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
           </Routes>
         </Main>
