6ae02899dcbae5df0de37029f6ec15cf6a80fe6e
lucashmunoz <lucashmunoz@gmail.com>
2023-11-24 01:32:00 -0300

    se agrega animacion del placeholder de quick search

--- src/pages/Home/QuickSearch/QuickSearch.jsx
+++ src/pages/Home/QuickSearch/QuickSearch.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import promoImg1 from "assets/promo-imgs/promo-imgs-banner_1.jpg";
 import Input from "components/Input";
@@ -15,6 +15,34 @@ import {
   SearchPromoText
 } from "./styles";
 
+const placeholders = [
+  "Ingresa lo que quieres aprender",
+  "Matemáticas",
+  "Inglés",
+  "Cocina Hogareña"
+];
+
+/**
+ * hook para poder usar un interval con estado de react
+ * https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+ */
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    const id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+};
+
 const QuickSearch = () => {
   const [searchValue, setSearchValue] = useState("");
   const nav = useNavigate();
@@ -24,6 +52,25 @@ const QuickSearch = () => {
     nav("/services", { state: { subject: searchValue.toString() } });
   };
 
+  const [placeholderValue, setPlaceholderValue] = useState(""); // el valor final del placeholder que se va a renderizar en el buscador
+  const [placeholderCursorIndex, setPlaceholderCursorIndex] = useState(0); // el indice del placeholder hasta donde hacer el corte
+  const [placeholderTextIndex, setPlaceholderTextIndex] = useState(0); // el indice de cual es el placeholder a recortar
+
+  useInterval(() => {
+    setPlaceholderValue(
+      placeholders[placeholderTextIndex].slice(0, placeholderCursorIndex)
+    ); // se calcula el placeholder a mostrar
+
+    if (placeholderCursorIndex >= placeholders[placeholderTextIndex].length) {
+      setPlaceholderTextIndex((prev) =>
+        prev + 1 < placeholders.length ? prev + 1 : 0
+      ); // seteando el indice correspondiente al proximo texto
+      setPlaceholderCursorIndex(0); // se reinicia el indice donde hacer el corte
+    } else {
+      setPlaceholderCursorIndex((prev) => prev + 1); // se aumenta en 1 el indice
+    }
+  }, 125);
+
   return (
     <QuickSearchWrapper>
       <SearchBoxContainer onSubmit={handleBuscarProfesor}>
@@ -32,7 +79,7 @@ const QuickSearch = () => {
           <SearchInputWrapper>
             <Input
               type="text"
-              placeholder="Ingresa lo que quieres aprender"
+              placeholder={placeholderValue}
               inputType="primary"
               onChangeHandler={(e) => {
                 setSearchValue(e.target.value);
